4520b74101d01a177fc7a59074c14422572c926d
Letus252 <RandomF0x@proton.me>
2024-09-09 14:41:58 +0200

    nordtheme patch + custom colors

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "CascadiaCodePL:pixelsize=18:antialias=true:autohint=true";
+static char *font = "CascadiaCodePL:pixelsize=22:antialias=true:autohint=true";
 //static char *font = "CascadiaCode:pixelsize=18:antialias=true:autohint=true";
 //static char *font = "UbuntuMono-B:pixelsize=20:antialias=true:autohint=true";
 //static char *font = "IBMPlexSans-Medium.ttf:pixelsize=18:antialias=true:autohint=true";
@@ -14,7 +14,7 @@ static char *font = "CascadiaCodePL:pixelsize=18:antialias=true:autohint=true";
 
 static int borderpx = 2;
 
-//alpha value for the alpha patch (def 8)
+//transparency for the alpha patch (default 0.8)
 float alpha = 0.7;
 
 /*
@@ -102,35 +102,34 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#1D2123", /* black   */
+	"#EF5350", /* red     */
+	"#4CAF50", /* green   */
+	"#F9D07D", /* yellow  */
+	"#234987", /* blue    */
+	"#FE00AC", /* magenta */
+	"#94F7FF", /* cyan    */
+	"#eceff4", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#323842", /* black   */
+	"#FF7874", /* red     */
+	"#6FF874", /* green   */
+	"#FFFFB7", /* yellow  */
+	"#366BC0", /* blue    */
+	"#FF00F4", /* magenta */
+	"#D2FFFF", /* cyan    */
+	"#FFFFFF", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#E5E5E5", /* default foreground colour */
+	"#000000", /* default background colour */
 };
 
 
@@ -138,10 +137,12 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
+
+
 
 /*
  * Default shape of cursor
